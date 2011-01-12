7f3ec4b11c859224e6203a087443c2a95d345ce1
rgrp <rufus.pollock@okfn.org>
2011-01-12 02:15:07 +0000

    [js][m]: finally get slider computation and pie charts working properly (very broken before).

--- openhdi/static/app.js
+++ openhdi/static/app.js
@@ -1,29 +1,32 @@
 var OpenHDI = (function($, my) {
 
-  my.getWeightings = function() {
-    var weightings = [];
-    $('.weighting').each(function(idx, div) {
-      var div = $(div);
-      weightings.push({ id: div.attr('id').split('-')[1], weighting: div.slider('value') });
-    });
-    return weightings;
-  };
-
   my.renderPieChart = function() {
-    var data = $('.weighting').map(function(idx, div) {
-      var div = $(div);
-      return { label: div.attr('id').split('-')[1], data: div.slider('value') }
+    var data = $('.question').map(function(idx, el) {
+      var sliderDiv = $(el).find('.weighting');
+      var questionLabel = $(el).find('h3').first().text();
+      return { label: questionLabel, data: sliderDiv.slider('value') }
     });
+
+    function shortenLabel(label) {
+      if(label && label.length>0) {
+        return label.split(' ').slice(0,3).join(' ');
+      } else {
+        return '';
+      }
+    }
 
     options = {
       series: {
         pie: { 
           show: true,
           label: {
-            show: false,
+            // show: false,
             radius: 3/4,
             formatter: function(label, series){
-                return '<div style="font-size:8pt;text-align:center;padding:2px;color:white;">'+label+'<br/>'+Math.round(series.percent)+'%</div>';
+                var out = '<div style="font-size:8pt;text-align:center;padding:2px;color:white;">'
+                  + shortenLabel(label)
+                  + '</div>';
+                return out;
             },
             background: { 
               opacity: 0.5,
@@ -63,7 +66,11 @@ var OpenHDI = (function($, my) {
   };
 
   my.setWeighting = function(questionId, val) {
-    $("#weighting-" + fieldId + '-percent').val(val);
+    $("#weighting-" + questionId + '-percent').val(val);
+  }
+
+  my.getWeighting = function(questionId) {
+    return $("#weighting-" + questionId + '-percent').val();
   }
 
   my.setupApp = function() {
@@ -73,44 +80,67 @@ var OpenHDI = (function($, my) {
         $(this).parent().slideUp(200);
       }).end()
 
-   $('.weighting').slider({
-      value: 100/$(".weighting").size(),
+    $('.weighting').slider({
+      value: 100,
       min: 0,
       max: 100,
       step: 1,
-      animate: true,
-      create: function( event, ui ) {
-        $(".weighting").each(function(i) {
-          var val = Math.floor(100 / $(".weighting").size());
-          fieldId = this.id.split('-')[1];
-          my.setWeighting(fieldId, val);
-        }); 
-      },
+      animate: 'fast',
       slide: function(event, ui) {
         questionId = event.target.id.split('-')[1];
+        var newValueThis = ui.value;
 
-        var sum = 0;
-        $(".weighting").each(function(s) {
+        var sum = newValueThis;
+        $(".weighting").not(this).each(function(s) {
             sum += $(this).slider('value');
         });
 
-        var sumOthers = sum - ui.value;
-        
-        $(".weighting").not(this).each(function(i) {
-            var newval = (100 - ui.value) * ($(this).slider('value') / sumOthers);
-          $(this).slider('value', newval);
+        // weights in smallest first order
+        var weightList = [];
+        $(".weighting").not(this).each(function(idx, el) {
+          var curValue = $(el).slider('value');
+          weightList.push([curValue, el.id]);
+        });
+        weightList.sort();
+
+        var toChange = 100 - sum;
+        // deduct/add from other value until nothing left
+        // we deduct/add equal amounts taking account of min/max
+        var numQuestions = $('.question').length-1;
+        $(weightList).each(function(idx, tuple) {
+          var curValue = tuple[0];
+          var _change = Math.round(toChange / (numQuestions));
+          var _change = Math.max(0-curValue, Math.min(_change, 100-curValue));
+          var newval = curValue + _change;
+          toChange -= _change; 
+          numQuestions -= 1;
+          $('#'+tuple[1]).slider('value', newval);
         });
         
-        $(".weighting").each(function(i) {
+        $(".weighting").not(this).each(function(idx, el) {
             var val = $(this).slider('value');
             fieldId = this.id.split('-')[1];
             my.setWeighting(fieldId, val);
         }); 
 
+        // this field
+        // do it separately so we use updated value
+        fieldId = this.id.split('-')[1];
+        my.setWeighting(fieldId, newValueThis);
+
         my.renderPieChart();
       }
     });
+
+    // set up slider from weights provided
+    $('.weighting-val').each(function(idx, el) {
+      fieldId = el.id.split('-')[1];
+      el = $(el);
+      $('#weighting-' + fieldId).slider('value', el.attr('value'));
+    });
   }
+
   return my;
+
 }(jQuery, OpenHDI || {}));
 
