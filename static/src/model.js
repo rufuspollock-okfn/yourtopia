8d54dc8b9988e764892f500e6670aee5ff9207f7
Marian Steinbach <marian@sendung.de>
2012-04-27 21:40:34 +0200

    Todays changes

--- static/src/model.js
+++ static/src/model.js
@@ -39,24 +39,41 @@ YOURTOPIA.Model = function($) {
    */
   my.Index = Backbone.Model.extend({
     defaults: {
+      id: null,
+      country: null,
+      version: null,
       user_name: null,
       user_url: null,
+      user_ip: null,
       description: null,
       created_at: null,
-      weighted_measures: ['employment', 'education',
-        'health', 'environment', 'equality',
-        'science', 'safety', 'social'],
-      employment_weight: 0.5,
-      education_weight: 0.5,
-      health_weight: 0.5,
-      environment_weight: 0.5,
-      equality_weight: 0.5,
-      science_weight: 0.5,
-      safety_weight: 0.5,
-      social_weight: 0.5
+      weighted_measures: null
     },
 
-    initialize: function() {}
+    /**
+     * If parameter data is given, the model is pre-populated
+     */
+    initialize: function(data) {
+      this.set({
+        weighted_measures: YOURTOPIA.config.indicator_categories,
+        country: YOURTOPIA.config.country,
+        version: YOURTOPIA.config.version
+      });
+      // create weight attributes according to configuration
+      for (var c in YOURTOPIA.config.indicator_categories) {
+        var category = YOURTOPIA.config.indicator_categories[c];
+        this.set(category + '_weight', YOURTOPIA.config.category_value_default);
+      }
+      // in case we have a 'weights' attribute (which means we got
+      // this pre-populated) we fill the values in the right places
+      if (this.has('weights')) {
+        var weights = JSON.parse(this.get('weights'));
+        for (var w in weights) {
+          this.set(w, weights[w]);
+        }
+        this.unset('weights');
+      }
+    }
   });
 
   return my;
