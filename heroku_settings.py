fe130a24cf77c4895122cf8b1cbcf43b45571466
Rufus Pollock <rufus.pollock@okfn.org>
2012-08-26 13:53:41 +0100

    [#42,settings][xs]: typo in last commit!.

--- heroku_settings.py
+++ heroku_settings.py
@@ -5,5 +5,5 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                             os.environ.get('SHARED_DATABASE_URL'))
 
-PORT = os.environt.get('PORT', 5000)
+PORT = os.environ.get('PORT', 5000)
 
