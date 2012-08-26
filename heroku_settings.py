4ecdd5d24d7c8e67873a98fe15c99d09b2c76bb8
Rufus Pollock <rufus.pollock@okfn.org>
2012-08-26 13:51:37 +0100

    [#42,settings][xs]: set port from environ option as heroku needs this.

--- heroku_settings.py
+++ heroku_settings.py
@@ -5,3 +5,5 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                             os.environ.get('SHARED_DATABASE_URL'))
 
+PORT = os.environt.get('PORT', 5000)
+
