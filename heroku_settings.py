d36dbd8266cfabdaa0000657de602fcd30e4af38
Daniel Fowler <me@danfowler.net>
2016-08-11 16:16:55 -0400

    make it better

--- heroku_settings.py
+++ heroku_settings.py
@@ -4,6 +4,5 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                             os.environ.get('SHARED_DATABASE_URL'))
-
 PORT = os.environ.get('PORT', 5000)
 
